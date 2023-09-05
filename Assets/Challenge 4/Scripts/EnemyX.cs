470fd4503ad0612e9b586821d8fbe8f5ab3341d2
LeuMax <91516151+Leumax90@users.noreply.github.com>
2023-09-05 19:03:57 +0300

    +

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+    private SpawnManagerX upSpeedEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        upSpeedEnemy = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerGoal = GameObject.FindWithTag("PlayerGoal");
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * (speed + (speed*upSpeedEnemy.waveCount) /6) * Time.deltaTime);
+         
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
